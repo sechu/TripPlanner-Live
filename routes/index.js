2ef938595a0ba3bc8a0c1b0ff0bfa841a2905205
sechu <sabrina.chu@gmail.com>
2016-05-11 15:32:52 -0400

    forEach choices implemented

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res) {
-  res.render('index');
+var Promise = require('bluebird');
+var db = require('../models');
+var Place = require('../models/place');
+var Hotel = require('../models/hotel');
+var Restaurant = require('../models/restaurant');
+var Activity = require('../models/activity');
+
+
+router.get('/', function(req, res, next) {
+	Promise.all([
+		Hotel.findAll(),
+		Restaurant.findAll(),
+		Activity.findAll()
+	])
+	.spread(function(hotels, restaurants, activities) {
+		res.render('index', {
+  			templateHotels: hotels,
+  			templateRestaurants: restaurants,
+  			templateActivities: activities
+  		});
+	})
+  	.catch(next);
 });
 
 module.exports = router;
